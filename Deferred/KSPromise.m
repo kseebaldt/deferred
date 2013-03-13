d13e5a3c5420607c475fbc6cbd0c84f8c690df74
Kurtis Seebaldt <kseebaldt@gmail.com>
2013-03-13 08:08:50 -0700

    reduce duplicated code

--- Deferred/KSPromise.m
+++ Deferred/KSPromise.m
@@ -63,36 +63,31 @@
     return promise;
 }
 
-- (KSPromise *)then:(promiseValueCallback)fulfilledCallback error:(promiseErrorCallback)errorCallback {
+- (KSPromise *)then:(promiseValueCallback)fulfilledCallback
+              error:(promiseErrorCallback)errorCallback {
     if (self.cancelled) return nil;
+    if (![self completed]) {
+        KSPromiseCallbacks *callbacks = [[KSPromiseCallbacks alloc] initWithFulfilledCallback:fulfilledCallback
+                                                                                errorCallback:errorCallback];
+        [self.callbacks addObject:callbacks];
+        return callbacks.childPromise;
+    }
+
+    id nextValue;
     if (self.fulfilled) {
-        id newValue = self.value;
+        nextValue = self.value;
         if (fulfilledCallback) {
-           newValue = fulfilledCallback(self.value);
+           nextValue = fulfilledCallback(self.value);
         }
-        KSPromise *promise = [[KSPromise alloc] init];
-        if ([newValue isKindOfClass:[NSError class]]) {
-            [promise rejectWithError:newValue];
-        } else {
-            [promise resolveWithValue:newValue];
-        }
-        return promise;
     } else if (self.rejected) {
-        id nextValue = self.error;
+        nextValue = self.error;
         if (errorCallback) {
             nextValue = errorCallback(self.error);
         }
-        KSPromise *promise = [[KSPromise alloc] init];
-        if ([nextValue isKindOfClass:[NSError class]]) {
-            [promise rejectWithError:nextValue];
-        } else {
-            [promise resolveWithValue:nextValue];
-        }
-        return promise;
     }
-    KSPromiseCallbacks *callbacks = [[KSPromiseCallbacks alloc] initWithFulfilledCallback:fulfilledCallback errorCallback:errorCallback];
-    [self.callbacks addObject:callbacks];
-    return callbacks.childPromise;
+    KSPromise *promise = [[KSPromise alloc] init];
+    [self resolvePromise:promise withValue:nextValue];
+    return promise;
 }
 
 - (void)cancel {
@@ -100,6 +95,62 @@
     [self.callbacks removeAllObjects];
 }
 
+- (void)resolveWithValue:(id)value {
+    if (self.completed || self.cancelled) return;
+    self.value = value;
+    self.fulfilled = YES;
+    for (KSPromiseCallbacks *callbacks in self.callbacks) {
+        id nextValue = self.value;
+        if (callbacks.fulfilledCallback) {
+            nextValue = callbacks.fulfilledCallback(value);
+        } else if (callbacks.deprecatedFulfilledCallback) {
+            callbacks.deprecatedFulfilledCallback(self);
+            continue;
+        }
+        [self resolvePromise:callbacks.childPromise withValue:nextValue];
+    }
+    [self finish];
+}
+
+- (void)rejectWithError:(NSError *)error {
+    if (self.completed || self.cancelled) return;
+    self.error = error;
+    self.rejected = YES;
+    for (KSPromiseCallbacks *callbacks in self.callbacks) {
+        id nextValue = self.error;
+        if (callbacks.errorCallback) {
+            nextValue = callbacks.errorCallback(error);
+        } else if (callbacks.deprecatedErrorCallback) {
+            callbacks.deprecatedErrorCallback(self);
+            continue;
+        }
+        [self resolvePromise:callbacks.childPromise withValue:nextValue];
+    }
+    [self finish];
+}
+
+- (void)resolvePromise:(KSPromise *)promise withValue:(id)value {
+    if ([value isKindOfClass:[NSError class]]) {
+        [promise rejectWithError:value];
+    } else {
+        [promise resolveWithValue:value];
+    }
+}
+
+- (void)finish {
+    for (KSPromiseCallbacks *callbacks in self.callbacks) {
+        if (callbacks.deprecatedCompleteCallback) {
+            callbacks.deprecatedCompleteCallback(self);
+        }
+    }
+    [self.callbacks removeAllObjects];
+}
+
+- (BOOL)completed {
+    return self.fulfilled || self.rejected;
+}
+
+#pragma mark - Deprecated methods
 - (void)whenResolved:(deferredCallback)callback {
     if (self.fulfilled) {
         callback(self);
@@ -128,61 +179,6 @@
         callbacks.deprecatedCompleteCallback = callback;
         [self.callbacks addObject:callbacks];
     }
-}
-
-- (void)resolveWithValue:(id)value {
-    if (self.completed || self.cancelled) return;
-    self.value = value;
-    self.fulfilled = YES;
-    for (KSPromiseCallbacks *callbacks in self.callbacks) {
-        id nextValue = self.value;
-        if (callbacks.fulfilledCallback) {
-            nextValue = callbacks.fulfilledCallback(value);
-        } else if (callbacks.deprecatedFulfilledCallback) {
-            callbacks.deprecatedFulfilledCallback(self);
-            continue;
-        }
-        if ([nextValue isKindOfClass:[NSError class]]) {
-            [callbacks.childPromise rejectWithError:nextValue];
-        } else {
-            [callbacks.childPromise resolveWithValue:nextValue];
-        }
-    }
-    [self finish];
-}
-
-- (void)rejectWithError:(NSError *)error {
-    if (self.completed || self.cancelled) return;
-    self.error = error;
-    self.rejected = YES;
-    for (KSPromiseCallbacks *callbacks in self.callbacks) {
-        id nextValue = self.error;
-        if (callbacks.errorCallback) {
-            nextValue= callbacks.errorCallback(error);
-        } else if (callbacks.deprecatedErrorCallback) {
-            callbacks.deprecatedErrorCallback(self);
-            continue;
-        }
-        if ([nextValue isKindOfClass:[NSError class]]) {
-            [callbacks.childPromise rejectWithError:nextValue];
-        } else {
-            [callbacks.childPromise resolveWithValue:nextValue];
-        }
-    }
-    [self finish];
-}
-
-- (void)finish {
-    for (KSPromiseCallbacks *callbacks in self.callbacks) {
-        if (callbacks.deprecatedCompleteCallback) {
-            callbacks.deprecatedCompleteCallback(self);
-        }
-    }
-    [self.callbacks removeAllObjects];
-}
-
-- (BOOL)completed {
-    return self.fulfilled || self.rejected;
 }
 
 #pragma mark - Private methods
