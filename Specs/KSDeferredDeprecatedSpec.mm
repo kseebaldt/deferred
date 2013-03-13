867d74c66c1c9e987b68ad381abc04f923bc835c
Kurtis Seebaldt <kseebaldt@gmail.com>
2013-03-12 23:04:22 -0700

    move specs for deprecated interface into its own spec

---
+++ Specs/KSDeferredDeprecatedSpec.mm
@@ -0,0 +1,213 @@
+#import <Cedar/SpecHelper.h>
+
+#import "KSPromise.h"
+#import "KSDeferred.h"
+
+using namespace Cedar::Matchers;
+
+SPEC_BEGIN(KSDeferredDeprecatedSpec)
+
+describe(@"KSDeferred", ^{
+    __block KSPromise *promise;
+    __block KSDeferred *deferred;
+    
+    beforeEach(^{
+        deferred = [KSDeferred defer];
+        promise = deferred.promise;
+    });
+    
+    it(@"should be created", ^{
+        deferred should_not be_nil;
+    });
+    
+    it(@"should have a promise", ^{
+        promise should_not be_nil;
+    });
+    
+    describe(@"when setting a success callback", ^{
+        __block BOOL called;
+        __block NSString *value;
+        
+        beforeEach(^{
+            called = NO;
+            value = nil;
+            [promise whenResolved:^(KSPromise *d) {
+                called = YES;
+                value = d.value;
+            }];
+        });
+        
+        it(@"should call the success callback when the promise gets a value", ^{
+            [deferred resolveWithValue:@"SUCCESS"];
+            called should be_truthy;
+            value should equal(@"SUCCESS");
+        });
+        
+        it(@"should not call the success callback when deferred is cancelled", ^{
+            [promise cancel];
+            [deferred resolveWithValue:@"SUCCESS"];
+            called should_not be_truthy;
+            value should_not equal(@"SUCCESS");
+        });
+        
+        describe(@"when adding an additional success callback", ^{
+            __block BOOL called2;
+            __block NSString *value2;
+            
+            beforeEach(^{
+                called2 = NO;
+                value2 = nil;
+                [promise whenResolved:^(KSPromise *d) {
+                    called2 = YES;
+                    value2 = d.value;
+                }];
+                [deferred resolveWithValue:@"SUCCESS"];
+            });
+            
+            it(@"should call the original success callback when the promise gets a value", ^{
+                called should be_truthy;
+                value should equal(@"SUCCESS");
+            });
+            
+            it(@"should call the additional success callback when the promise gets a value", ^{
+                called2 should be_truthy;
+                value2 should equal(@"SUCCESS");
+            });
+        });
+    });
+    
+    describe(@"when adding a success callback after a success value has been set", ^{
+        __block BOOL called = NO;
+        __block NSString *value = nil;
+        
+        it(@"should call the success callback when the promise gets a value", ^{
+            called should_not be_truthy;
+            value should_not equal(@"SUCCESS");
+            
+            [deferred resolveWithValue:@"SUCCESS"];
+            
+            [promise whenResolved:^(KSPromise *d) {
+                called = YES;
+                value = d.value;
+            }];
+            called should be_truthy;
+            value should equal(@"SUCCESS");
+        });
+    });
+    
+    describe(@"when setting an error callback", ^{
+        __block BOOL called = NO;
+        __block NSError *error = nil;
+        
+        beforeEach(^{
+            called = NO;
+            error = nil;
+            
+            [promise whenRejected:^(KSPromise *p) {
+                error = p.error;
+                called = YES;
+            }];
+        });
+        
+        it(@"should call the error callback when the promise gets an error", ^{
+            [deferred rejectWithError:[NSError errorWithDomain:@"FAIL" code:123 userInfo:nil]];
+            called should be_truthy;
+            error.domain should equal(@"FAIL");
+        });
+        
+        it(@"should not call the callback after its cancelled", ^{
+            [promise cancel];
+            [deferred rejectWithError:[NSError errorWithDomain:@"FAIL" code:123 userInfo:nil]];
+            called should_not be_truthy;
+            error.domain should_not equal(@"FAIL");
+        });
+        
+        describe(@"when adding an additional error callback", ^{
+            __block BOOL called2 = NO;
+            __block NSError *error2 = nil;
+            
+            beforeEach(^{
+                [promise whenRejected:^(KSPromise *p) {
+                    error2 = p.error;
+                    called2 = YES;
+                }];
+                
+                [deferred rejectWithError:[NSError errorWithDomain:@"FAIL" code:123 userInfo:nil]];
+            });
+            
+            it(@"should call the error callback when the promise gets an error", ^{
+                called should be_truthy;
+                error.domain should equal(@"FAIL");
+            });
+            
+            it(@"should call the error callback when the promise gets an error", ^{
+                called2 should be_truthy;
+                error2.domain should equal(@"FAIL");
+            });
+        });
+    });
+    
+    describe(@"when setting a complete callback", ^{
+        __block BOOL called = NO;
+        __block NSString *value = nil;
+        __block NSError *error = nil;
+        
+        beforeEach(^{
+            [promise whenFulfilled:^(KSPromise *p) {
+                called = YES;
+                value = p.value;
+                error = p.error;
+            }];
+        });
+        
+        it(@"should call the complete callback when the promise gets a value", ^{
+            [deferred resolveWithValue:@"SUCCESS"];
+            called should be_truthy;
+            value should equal(@"SUCCESS");
+            error should be_nil;
+        });
+        
+        it(@"should call the complete callback when the promise gets an error", ^{
+            [deferred rejectWithError:[NSError errorWithDomain:@"FAIL" code:123 userInfo:nil]];
+            called should be_truthy;
+            value should be_nil;
+            error.domain should equal(@"FAIL");
+        });
+        
+        describe(@"when adding an additional complete callback", ^{
+            __block BOOL called2 = NO;
+            __block NSString *value2 = nil;
+            __block NSError *error2 = nil;
+            
+            beforeEach(^{
+                [promise whenFulfilled:^(KSPromise *p) {
+                    called2 = YES;
+                    value2 = p.value;
+                    error2 = p.error;
+                }];
+            });
+            
+            it(@"should call both callback on success", ^{
+                [deferred resolveWithValue:@"SUCCESS"];
+                called should be_truthy;
+                value should equal(@"SUCCESS");
+                error should be_nil;
+                called2 should be_truthy;
+                value2 should equal(@"SUCCESS");
+                error2 should be_nil;
+            });
+            
+            it(@"should call both callback on error", ^{
+                [deferred rejectWithError:[NSError errorWithDomain:@"FAIL" code:123 userInfo:nil]];
+                called should be_truthy;
+                value should be_nil;
+                error.domain should equal(@"FAIL");
+                called2 should be_truthy;
+                value2 should be_nil;
+                error2.domain should equal(@"FAIL");
+            });
+        });
+    });
+});
+
+SPEC_END
--- Specs/KSDeferredSpec.mm
+++ Specs/KSDeferredSpec.mm
@@ -15,7 +15,7 @@ describe(@"KSDeferred", ^{
         deferred = [KSDeferred defer];
         promise = deferred.promise;
     });
-    
+
     it(@"should be created", ^{
         deferred should_not be_nil;
     });
@@ -23,212 +23,27 @@ describe(@"KSDeferred", ^{
     it(@"should have a promise", ^{
         promise should_not be_nil;
     });
-    
-    describe(@"when setting a success callback", ^{
-        __block BOOL called;
-        __block NSString *value;
-        
-        beforeEach(^{
-            called = NO;
-            value = nil;
-            [promise whenResolved:^(KSPromise *d) {
-                called = YES;
-                value = d.value;
-            }];
-        });
-        
-        it(@"should call the success callback when the promise gets a value", ^{
-            [deferred resolveWithValue:@"SUCCESS"];
-            called should be_truthy;
-            value should equal(@"SUCCESS");
-        });
-        
-        it(@"should not call the success callback when deferred is cancelled", ^{
-            [promise cancel];
-            [deferred resolveWithValue:@"SUCCESS"];
-            called should_not be_truthy;
-            value should_not equal(@"SUCCESS");
-        });
-        
-        describe(@"when adding an additional success callback", ^{
-            __block BOOL called2;
-            __block NSString *value2;
-            
-            beforeEach(^{
-                called2 = NO;
-                value2 = nil;
-                [promise whenResolved:^(KSPromise *d) {
-                    called2 = YES;
-                    value2 = d.value;
-                }];
-                [deferred resolveWithValue:@"SUCCESS"];
-            });
-            
-            it(@"should call the original success callback when the promise gets a value", ^{
-                called should be_truthy;
-                value should equal(@"SUCCESS");
-            });
-            
-            it(@"should call the additional success callback when the promise gets a value", ^{
-                called2 should be_truthy;
-                value2 should equal(@"SUCCESS");
-            });
-        });
-    });
-    
-    describe(@"when adding a success callback after a success value has been set", ^{
-        __block BOOL called = NO;
-        __block NSString *value = nil;
-        
-        it(@"should call the success callback when the promise gets a value", ^{
-            called should_not be_truthy;
-            value should_not equal(@"SUCCESS");
-
-            [deferred resolveWithValue:@"SUCCESS"];
-
-            [promise whenResolved:^(KSPromise *d) {
-                called = YES;
-                value = d.value;
-            }];
-            called should be_truthy;
-            value should equal(@"SUCCESS");
-        });
-    });
-    
-    describe(@"when setting an error callback", ^{
-        __block BOOL called = NO;
-        __block NSError *error = nil;
-        
-        beforeEach(^{
-            called = NO;
-            error = nil;
-
-            [promise whenRejected:^(KSPromise *p) {
-                error = p.error;
-                called = YES;
-            }];
-        });
-        
-        it(@"should call the error callback when the promise gets an error", ^{
-            [deferred rejectWithError:[NSError errorWithDomain:@"FAIL" code:123 userInfo:nil]];
-            called should be_truthy;
-            error.domain should equal(@"FAIL");
-        });
-        
-        it(@"should not call the callback after its cancelled", ^{
-            [promise cancel];
-            [deferred rejectWithError:[NSError errorWithDomain:@"FAIL" code:123 userInfo:nil]];
-            called should_not be_truthy;
-            error.domain should_not equal(@"FAIL");
-        });
-        
-        describe(@"when adding an additional error callback", ^{
-            __block BOOL called2 = NO;
-            __block NSError *error2 = nil;
-            
-            beforeEach(^{
-                [promise whenRejected:^(KSPromise *p) {
-                    error2 = p.error;
-                    called2 = YES;
-                }];
-                
-                [deferred rejectWithError:[NSError errorWithDomain:@"FAIL" code:123 userInfo:nil]];
-            });
-            
-            it(@"should call the error callback when the promise gets an error", ^{
-                called should be_truthy;
-                error.domain should equal(@"FAIL");
-            });
-            
-            it(@"should call the error callback when the promise gets an error", ^{
-                called2 should be_truthy;
-                error2.domain should equal(@"FAIL");
-            });
-        });        
-    });
-    
-    describe(@"when setting a complete callback", ^{
-        __block BOOL called = NO;
-        __block NSString *value = nil;
-        __block NSError *error = nil;
-        
-        beforeEach(^{
-            [promise whenFulfilled:^(KSPromise *p) {
-                called = YES;
-                value = p.value;
-                error = p.error;
-            }];
-        });
-        
-        it(@"should call the complete callback when the promise gets a value", ^{
-            [deferred resolveWithValue:@"SUCCESS"];
-            called should be_truthy;
-            value should equal(@"SUCCESS");
-            error should be_nil;
-        });
-        
-        it(@"should call the complete callback when the promise gets an error", ^{
-            [deferred rejectWithError:[NSError errorWithDomain:@"FAIL" code:123 userInfo:nil]];
-            called should be_truthy;
-            value should be_nil;
-            error.domain should equal(@"FAIL");
-        });
-        
-        describe(@"when adding an additional complete callback", ^{
-            __block BOOL called2 = NO;
-            __block NSString *value2 = nil;
-            __block NSError *error2 = nil;
-            
-            beforeEach(^{
-                [promise whenFulfilled:^(KSPromise *p) {
-                    called2 = YES;
-                    value2 = p.value;
-                    error2 = p.error;
-                }];
-            });
-            
-            it(@"should call both callback on success", ^{
-                [deferred resolveWithValue:@"SUCCESS"];
-                called should be_truthy;
-                value should equal(@"SUCCESS");
-                error should be_nil;
-                called2 should be_truthy;
-                value2 should equal(@"SUCCESS");
-                error2 should be_nil;
-            });
-            
-            it(@"should call both callback on error", ^{
-                [deferred rejectWithError:[NSError errorWithDomain:@"FAIL" code:123 userInfo:nil]];
-                called should be_truthy;
-                value should be_nil;
-                error.domain should equal(@"FAIL");
-                called2 should be_truthy;
-                value2 should be_nil;
-                error2.domain should equal(@"FAIL");
-            });
-        });
-    });
 
     describe(@"joining", ^ {
         __block KSPromise *promise2;
         __block KSDeferred *deferred2;
         __block KSPromise *joinedPromise;
-        __block BOOL resolveCalled;
-        __block BOOL rejectCalled;
+        __block BOOL fulfilled;
+        __block BOOL rejected;
 
         beforeEach(^{
             deferred2 = [KSDeferred defer];
             promise2 = deferred2.promise;
             joinedPromise = [KSPromise join:[NSArray arrayWithObjects:promise, promise2, nil]];
 
-            resolveCalled = NO;
-            [joinedPromise whenResolved:^(KSPromise *p) {
-                resolveCalled = YES;
-            }];
+            fulfilled = rejected = NO;
 
-            rejectCalled = NO;
-            [joinedPromise whenRejected:^(KSPromise *p) {
-                rejectCalled = YES;
+            [joinedPromise then:^id(id value) {
+                fulfilled = YES;
+                return value;
+            } error:^id(NSError *error) {
+                rejected = YES;
+                return error;
             }];
         });
 
@@ -238,8 +53,8 @@ describe(@"KSDeferred", ^{
             });
 
             it(@"should not resolve the joined promise", ^{
-                resolveCalled should_not be_truthy;
-                rejectCalled should_not be_truthy;
+                fulfilled should_not be_truthy;
+                rejected should_not be_truthy;
             });
 
             describe(@"when both promises are resolved", ^{
@@ -248,7 +63,7 @@ describe(@"KSDeferred", ^{
                 });
 
                 it(@"should call the resolved callback", ^{
-                    resolveCalled should be_truthy;
+                    fulfilled should be_truthy;
                 });
 
                 it(@"should be able to read the resolved values of the joined promises", ^{
@@ -264,7 +79,7 @@ describe(@"KSDeferred", ^{
                 });
 
                 it(@"should call the rejected callback", ^{
-                    rejectCalled should be_truthy;
+                    rejected should be_truthy;
                 });
 
                 it(@"should be able to read the resolved values of the joined promises", ^{
@@ -283,8 +98,8 @@ describe(@"KSDeferred", ^{
             });
 
             it(@"should not reject the joined promise", ^{
-                resolveCalled should_not be_truthy;
-                rejectCalled should_not be_truthy;
+                fulfilled should_not be_truthy;
+                rejected should_not be_truthy;
             });
         });
     });
