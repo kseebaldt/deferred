6789555652bc78531bda4fa57e9e8292d8d319a1
Rachel Brindle <rbrindle@pivotal.io>
2015-05-15 10:57:53 -0700

    Add Carthage support

---
+++ Deferred-OSX-Framework/Deferred-OSX-Framework.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double Deferred_OSX_FrameworkVersionNumber;
+
+FOUNDATION_EXPORT const unsigned char Deferred_OSX_FrameworkVersionString[];
+
+#import <KSDeferred/KSCancellable.h>
+#import <KSDeferred/KSDeferred.h>
+#import <KSDeferred/KSPromise.h>
---
+++ Deferred-iOS-Framework/Deferred-iOS-Framework.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double Deferred_iOS_FrameworkVersionNumber;
+
+FOUNDATION_EXPORT const unsigned char Deferred_iOS_FrameworkVersionString[];
+
+#import <KSDeferred/KSCancellable.h>
+#import <KSDeferred/KSDeferred.h>
+#import <KSDeferred/KSPromise.h>
