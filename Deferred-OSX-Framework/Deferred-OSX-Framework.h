be81ab49dc7948f481d2d3d81f91e98f6be233ad
Rachel Brindle <rbrindle@pivotal.io>
2015-05-18 11:12:41 -0700

    remove unneed Deferred-$PLATFORM-Framework.h header

--- Deferred-OSX-Framework/Deferred-OSX-Framework.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-FOUNDATION_EXPORT double Deferred_OSX_FrameworkVersionNumber;
-
-FOUNDATION_EXPORT const unsigned char Deferred_OSX_FrameworkVersionString[];
-
-#import <KSDeferred/KSCancellable.h>
-#import <KSDeferred/KSDeferred.h>
-#import <KSDeferred/KSPromise.h>
--- Deferred-iOS-Framework/Deferred-iOS-Framework.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-FOUNDATION_EXPORT double Deferred_iOS_FrameworkVersionNumber;
-
-FOUNDATION_EXPORT const unsigned char Deferred_iOS_FrameworkVersionString[];
-
-#import <KSDeferred/KSCancellable.h>
-#import <KSDeferred/KSDeferred.h>
-#import <KSDeferred/KSPromise.h>
