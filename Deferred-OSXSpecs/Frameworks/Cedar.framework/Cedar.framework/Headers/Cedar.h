4ef09a9e71e7336daff053f5a00218a1b259b7e4
Brian Croom <bcroom@pivotal.io>
2015-09-17 16:22:04 -0400

    Miscellaneous updates for Xcode 7 support
    - Update to a newer Cedar version
    - Run the settings update wizard in Xcode
    - Clean up the build steps for the Spec targets

--- Deferred-OSXSpecs/Frameworks/Cedar.framework/Cedar.framework/Headers/Cedar.h
+++
@@ -1,2 +0,0 @@
-#import "CDRFunctions.h"
-#import "CDRSpecHelper.h"
--- Deferred-OSXSpecs/Frameworks/Cedar.framework/Versions/Current/Headers/ComparatorsBase.h
+++
@@ -1,9 +0,0 @@
-#import "CompareEqual.h"
-#import "CompareGreaterThan.h"
-#import "CompareCloseTo.h"
-
-#if TARGET_OS_IPHONE
-#import "UIGeometryCompareEqual.h"
-#else
-#import "OSXGeometryCompareEqual.h"
-#endif
